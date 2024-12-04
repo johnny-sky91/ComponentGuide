8529c84100df6d0c8d9af596ddcd2a9ffc8ed135
johnny-sky91 <73224128+johnny-sky91@users.noreply.github.com>
2024-12-04 10:28:30 +0100

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+from config import Config
+from .extensions import db, migrate
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Initialize extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
