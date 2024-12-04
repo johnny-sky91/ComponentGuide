e5629b36ddf061490228eb2ba5c25dffa03ab82e
johnny-sky91 <73224128+johnny-sky91@users.noreply.github.com>
2024-12-04 12:12:32 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template
-
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_moment import Moment
 from config import Config
-from .extensions import db, migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+moment = Moment()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-
-    # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
+    moment.init_app(app)
+
+    with app.app_context():
+        from . import routes, models
 
     return app
