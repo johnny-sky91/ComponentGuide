584e4527c89eb053ed7c31af36dd229926f94094
johnny-sky91 <73224128+johnny-sky91@users.noreply.github.com>
2024-12-05 10:11:18 +0100

    update in Component, added tables_dict

--- app/models.py
+++ app/models.py
@@ -11,12 +11,17 @@ class Component(db.Model):
     material_description = db.Column(db.String(64))
     supplier_material_number = db.Column(db.Integer, unique=True)
     manufacturer_part = db.Column(db.String(64), unique=True)
-    component_type = db.Column(db.String(64), default=None)
+    leadtime = db.Column(db.Integer, default=0)
+    component_type = db.Column(db.String(64), default=None)  # TODO to remove?
     status = db.Column(db.String(64), default=None)
     note = db.Column(db.String(160), default=None)
     check = db.Column(db.Boolean, default=False)
     on_shortage = db.Column(db.Boolean, default=False)
-
+    incoming_shipments_qty = db.Column(db.Integer, default=0)
+    free_to_order_qty = db.Column(db.Integer, default=0)
+    supplier_stock_qty = db.Column(db.Integer, default=0)
+    open_po_qty = db.Column(db.Integer, default=0)
+    unit_price = db.Column(db.Float, default=0.0)
     comments = db.relationship("ComponentComment", backref="component", lazy=True)
 
     def __repr__(self):
