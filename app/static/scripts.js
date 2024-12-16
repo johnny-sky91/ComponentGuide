e0b91fea5dcbeb488cf37cdba8f61ec63defad78
johnny-sky91 <73224128+johnny-sky91@users.noreply.github.com>
2024-12-16 07:26:14 +0100

    added toggleTable

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -8,4 +8,15 @@ function copyListToClipboard(list) {
     document.execCommand('copy');
     document.body.removeChild(textarea);
   }
-  
+
+  function toggleTable(tableId, textId) {
+    var table = document.getElementById(tableId);
+    var text = document.getElementById(textId);
+    if (table.style.display === "none") {
+        table.style.display = "table";
+        text.innerHTML = "Hide the table";
+    } else {
+        table.style.display = "none";
+        text.innerHTML = "Show the table";
+    }
+}
